cd9856d0bd8406d4744a9f0b1f8ed2bbd08680a4
LimRaynor <morden9137@gmail.com>
2026-02-23 14:25:22 +0900

    JENKINSA

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -18,7 +18,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "jenkins pipeline success check";
+        return "jenkins pipeline success check!!@!!@";
     }
 
     @GetMapping("/plus")
